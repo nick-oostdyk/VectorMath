4941eb3609c81284d05151b028bc9140e74ecaf9
Nick Oostdyk <NicholasOostdyk@gmail.com>
2021-02-07 00:22:47 -0500

    last testing commit

--- VectorMath/Main.cpp
+++ VectorMath/Main.cpp
@@ -3,9 +3,20 @@
 
 int main() { 
 
-	Vector a(10, 10, 10), b(1, 2, 3);
+	Vector
+		a(3, 10, 7),
+		b(8, -7, 4),
+		c(2, 1, 0),
+		d(-1, 2, 9),
+		e(3, -4, 5);
 
-	std::cout << a - b << std::endl;
+
+	std::cout << (c.Dot(d)) << std::endl;
+	std::cout << (d.Dot(c)) << std::endl;
+	std::cout << "cannot" << std::endl;
+	std::cout << (c.Cross(e).Dot(c)) << std::endl;
+	std::cout << "cannot" << std::endl;
+	std::cout << (a.Cross(b).Cross(c)) << std::endl;
 
 	return 0;
 }
